60e88be05eb578f2cebbac2502f34f31d4d82d03
aqreed <andres.quezada.reed@gmail.com>
2019-07-27 00:50:20 +0200

    added setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="solarradiation",
+    author='aqreed',
+    version="0.1dev0",
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=['numpy', 'matplotlib'],
+    tests_requires=['pytest']
+    )
