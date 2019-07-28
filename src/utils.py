35507367483179b799bb59196e3c7fb4220ec5cb
aqreed <andres.quezada.reed@gmail.com>
2019-07-28 17:22:36 +0200

    created utils file. Moved some functions from solar_radiation to utils

---
+++ src/utils.py
@@ -0,0 +1,158 @@
+# coding: utf-8
+
+"""
+    Utilities for solar radiation calculations, based on Duffie,
+    J.A., and Beckman, W. A., 1974, "Solar energy thermal processes"
+"""
+
+import numpy as np
+from numpy import sin, cos, tan, deg2rad
+
+
+def check_nth_day_range(n):
+    """
+    Checks whether the input nth_day of the year is within range
+
+    Parameters
+    ----------
+    n : integer
+        day of the year (1 to 365)
+
+    Returns
+    -------
+    None. Raises an exception in case
+    """
+    if isinstance(n, np.ndarray) and ((n < 1).any() or (n > 365).any()):
+            raise ValueError('n should be 1 <= n <= 365')
+    elif isinstance(n, int) and ((n < 1) or (n > 365)):
+            raise ValueError('n should be 1 <= n <= 365')
+
+    return None
+
+
+def check_latitude_range(lat):
+    """
+    Checks whether the input latitude is within range
+
+    Parameters
+    ----------
+    lat : float
+        latitude (-90 to 90) in degrees
+
+    Returns
+    -------
+    None. Raises an exception in case
+    """
+    if isinstance(lat, np.ndarray) and ((abs(lat) > 90).any()):
+            raise ValueError('latitude should be -90 <= latitude <= 90')
+    elif isinstance(lat, int) and (abs(lat) > 90):
+            raise ValueError('latitude should be -90 <= latitude <= 90')
+
+    return None
+
+
+def check_longitude_range(lng):
+    """
+    Checks whether the input longitude is within range
+
+    Parameters
+    ----------
+    lng : float
+        longitude (0 to 359) in degrees
+
+    Returns
+    -------
+    None. Raises an exception in case
+    """
+    if isinstance(lng, np.ndarray) and ((lng < 0).any() or (lng > 359).any()):
+            raise ValueError('longitude should be 0 <= longitude <= 359')
+    elif isinstance(lng, int) and ((lng < 0) or (lng > 359)):
+            raise ValueError('longitude should be 0 <= longitude <= 359')
+
+    return None
+
+
+class NoSunsetNoSunrise(Exception):
+    """
+    Raised when the latitude in question is in permanent night or day
+    """
+    def __init__(self):
+        self.msg = "Permanent night (or day) on this latitude on this day"
+
+    def __str__(self):
+        return repr(self.msg)
+
+
+def lla2ecef(lat, lng, h):
+    """
+    Calculates geocentric coordinates (ECEF - Earth Centered, Earth Fixed) for
+    a given set of latitude, longitude and altitude inputs.
+
+    Parameters
+    ----------
+    lat : float
+        latitude in degrees
+    lng : float
+        longitude in degrees
+    h : float
+        altitude above sea level in feet
+
+    Returns
+    -------
+    array-like
+        ECEF coordinates in meters
+    """
+    check_latitude_range(lat)
+    check_longitude_range(lng)
+
+    a = 6378137  # [m] Earth equatorial axis
+    b = 6356752.3142  # [m] Earth polar axis
+    e = 0.081819190842622  # Earth eccentricity
+
+    lat = deg2rad(lat)  # degrees to radians
+    lng = deg2rad(lng)  # degrees to radians
+    h = h * 0.3048  # feets to meters
+
+    N = a / (1 - (e * sin(lat))**2)**(.5)
+
+    x = (N + h) * cos(lat) * cos(lng)
+    y = (N + h) * cos(lat) * sin(lng)
+    z = (((b/a)**2) * N + h) * sin(lat)
+
+    return np.array([x, y, z])
+
+
+def ned2ecef(v_ned, lat, lng):
+    """
+    Converts vector from local geodetic horizon reference frame (NED - North,
+    East, Down) at a given latitude and longitude to geocentric coordinates
+    (ECEF - Earth Centered, Earth Fixed).
+
+    Parameters
+    ----------
+    v_ned: array-like
+        vector expressed in NED coordinates
+    lat : float
+        latitude in degrees
+    lng : float
+        longitude in degrees
+
+    Returns
+    -------
+    v_ecef : array-like
+        vector expressed in ECEF coordinates
+    """
+    check_latitude_range(lat)
+    check_longitude_range(lng)
+
+    lat = deg2rad(lat)
+    lng = deg2rad(lng)
+
+    Lne = np.array([[-sin(lat) * cos(lng), -sin(lat) * sin(lng), cos(lat)],
+                    [-sin(lng), cos(lng), 0],
+                    [-cos(lat) * cos(lng), -cos(lat) * sin(lng), -sin(lat)]])
+
+    Len = Lne.transpose()
+    v_ecef = Len.dot(v_ned)
+
+    return v_ecef
