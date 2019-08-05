39ef5066c6fa1c395d70db0b6bdf8ab9f73af790
aqreed <andres.quezada.reed@gmail.com>
2019-08-05 09:48:49 +0200

    moved dependencies to "requirements.txt" file. Updated travis yml file

--- setup.py
+++ setup.py
@@ -6,6 +6,5 @@ setup(
     version="0.1.dev0",
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    install_requires=['numpy', 'matplotlib', 'scikit-aero'],
     tests_requires=['pytest']
     )
