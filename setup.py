247c7dbd713de94820f4c0f0053a77ffe5362646
aqreed <andres.quezada.reed@gmail.com>
2019-08-05 10:07:28 +0200

    added description and url to the setup file. Minor edit to numpy version in requirements

--- setup.py
+++ setup.py
@@ -3,7 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name="solarradiation",
     author='aqreed',
+    description='Solar radiation model based on Duffie & Beckman\
+                "Solar energy thermal processes" (1974)',
     version="0.1.dev0",
+    url='https://github.com/aqreed/pysolarRadiation',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     tests_requires=['pytest']
