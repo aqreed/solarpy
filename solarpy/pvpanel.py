0c800be740d843751ad77bc968b6f71bcc9861a0
aqreed <andres.quezada.reed@gmail.com>
2019-08-22 17:52:45 +0200

    added "solar_panel" class

---
+++ solarpy/pvpanel.py
@@ -0,0 +1,77 @@
+# coding: utf-8
+
+"""
+    Photovoltaic panel class
+"""
+
+import numpy as np
+from datetime import datetime, timedelta
+from solarpy.radiation import *
+from solarpy.utils import *
+
+
+class solar_panel(object):
+    """
+    Photovoltaic solar panel class
+
+    Parameters
+    ----------
+    s : float
+        panel surface in m2
+    eff : float
+        panel efficiency
+    """
+
+    def __init__(self, s, eff):
+        self.eff = 0.32
+
+        if (s >= 0):
+            self.s = s
+        else:
+            raise ValueError('surface must be s >= 0')
+
+    def set_position(self, lat, lng, h):
+        """
+        Sets LLA position (latitude, longitude, altitude)
+
+        Parameters
+        ----------
+        lat : float
+            latitude (-90 to 90) in degrees
+        lng : float
+            longitude, (-180 to 180) in degrees
+        h : float
+            altitude above sea level in meters
+        """
+        self.lat = lat
+        self.lng = lng
+        self.h = h
+
+    def set_orientation(self, vnorm):
+        """
+        Sets the orientation of the panel in NED frame
+
+        Parameters
+        ----------
+        vnorm : array-like
+            unit vector normal to plane in NED frame
+        """
+        self.vnorm = vnorm
+
+    def set_datetime(self, date):
+        """
+        Sets date and time
+
+        Parameters
+        ----------
+        date : datetime object
+            date and *solar* time
+        """
+        self.date = date
+
+    def power(self):
+        """
+        Returns the output power of a solar panel
+        """
+        return irradiance_on_plane(self.vnorm, self.h,
+                                   self.date, self.lat) * self.s * self.eff
