79ad7813659d6f08a9811ac468e9de6a13b2837c
aqreed <andres.quezada.reed@gmail.com>
2019-09-23 15:56:46 +0200

    added classifiers to setup file

--- setup.py
+++ setup.py
@@ -14,5 +14,20 @@ setup(
     url='https://github.com/aqreed/solarpy',
     packages=['solarpy'],
     install_requires=['numpy', 'matplotlib'],
-    tests_requires=['pytest']
+    tests_requires=['pytest'],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Home Automation",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Physics",
+        "Topic :: Scientific/Engineering :: Visualization"
+    ]
     )
